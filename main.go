ee76ca66399f0f4598bd50263e1a6a02e9bf112b
FelixJobson <felix.jobson@tiki-institut.com>
2023-06-30 22:36:06 +0200

    root handler

--- main.go
+++ main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
 )
+
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "root url")
+}
+
+func world(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
-func world(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "World!")
+
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called - " + name)
+		h(w, r)
+	}
 }
 
 func main() {
@@ -17,8 +32,9 @@ func main() {
 		Addr: "127.0.0.1:8080",
 	}
 
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/world", world)
+	http.HandleFunc("/", log(root))
+	http.HandleFunc("/hello", log(hello))
+	http.HandleFunc("/world", log(world))
 
 	server.ListenAndServe()
 }
