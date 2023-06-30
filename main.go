0afab8ecf365a6d608150add37ac747c4911ea5a
FelixJobson <felix.jobson@tiki-institut.com>
2023-06-30 22:24:22 +0200

    use several handlers

--- main.go
+++ main.go
@@ -5,18 +5,27 @@ import (
 	"net/http"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
-	fmt.Println("got request")
+type HelloHandler struct{}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
 }
 
+type WorldHandler struct{}
+
+func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
+}
 func main() {
-	http.HandleFunc("/", handler)
+	hello := HelloHandler{}
+	world := WorldHandler{}
 
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: nil,
+		Addr: "127.0.0.1:8080",
 	}
+
+	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
 
 	server.ListenAndServe()
 }
